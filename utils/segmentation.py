1198fd57143640cac865a1374d49ad7b0e5b3819
En Qing <90186576+Enqing07@users.noreply.github.com>
2026-04-01 21:12:45 +0800

    Update segmentation.py

--- utils/segmentation.py
+++ utils/segmentation.py
@@ -12,7 +12,7 @@ def load_models():
     custom_model = models.CellposeModel(gpu=False)
 
     # Load the saved weights
-    weights_path = "model/cellpose_HITL_weights"
+    weights_path = "model/cellpose_HITL"
     custom_model.net.load_state_dict(torch.load(weights_path, map_location="cpu"))
 
     cyto3_model = models.Cellpose(
