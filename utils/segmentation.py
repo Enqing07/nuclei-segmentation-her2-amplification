8ea86216b6ef2cc445aef4d7f41ad1a827211408
En Qing <90186576+Enqing07@users.noreply.github.com>
2026-04-01 21:57:08 +0800

    Update segmentation.py

--- utils/segmentation.py
+++ utils/segmentation.py
@@ -5,15 +5,15 @@ import torch
 
 # Load BOTH models
 def load_models():
-    # custom_model = models.CellposeModel(
-    #     gpu=False,
-    #     pretrained_model="model/cellpose_HITL"
-    # )
+    custom_model = models.CellposeModel(
+        gpu=False,
+        pretrained_model="model/cellpose_HITL.pt"
+    )
 
-    custom_model = models.CellposeModel(gpu=False)
-    weights_path = "model/cellpose_HITL_weights.pt"
-    state_dict = torch.load(weights_path, map_location="cpu")
-    custom_model.net.load_state_dict(state_dict)
+    # custom_model = models.CellposeModel(gpu=False)
+    # weights_path = "model/cellpose_HITL_weights.pt"
+    # state_dict = torch.load(weights_path, map_location="cpu")
+    # custom_model.net.load_state_dict(state_dict)
 
 
     cyto3_model = models.Cellpose(
